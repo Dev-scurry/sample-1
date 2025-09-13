955376d2598c1467219d25e280e80a313e01207a
Dev-scurry <femi.daniel05@gmail.com>
2025-09-13 17:22:23 +0100

    java

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,21 @@ total = x + y
 console.log("this is what i calculated"+z)
 
 
+let bulbHolder = document.getElementById("bulb-holder");
+let onSwitch = document.getElementById('on-button');
+let offSwitch = document.getElementById('off-button');
+
+function switchBulbon(){
+    bulbHolder.src='./Assets/light on.jpg'
+}
+function switchBulboff(){
+    bulbHolder.src='./Assets/light off.jpg'
+}
+
+onSwitch.addEventListener('click', switchBulbon)
+
+
+offSwitch.addEventListener('click', switchBulboff)
 
 
 
